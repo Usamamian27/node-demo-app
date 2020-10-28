a02f291187252d6a3392e81323156b4eeb294afa
usama-mian-27 <muhammad.usama@neuralsoftsolutions.com>
2020-10-28 16:23:02 +0500

    Single route node demo

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  return res.send({ message: "Hello World" });
+});
+
+app.listen(PORT, () => console.log("Server is dancing on ", PORT));
